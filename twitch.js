8064806921bffd84e697f6ed4fa398f51218b186
Vincent Samaco <vsamaco@gmail.com>
2012-10-22 17:43:35 -0700

    created streams endpoint

--- twitch.js
+++ twitch.js
@@ -1,6 +1,9 @@
 var application_root = __dirname,
-    express = require("express"),
-    path = require("path");
+    path = require('path'),
+    http = require('http'),
+    url = require('url'),
+    request = require('request'),
+    express = require('express');
 
 var app = express();
 
@@ -14,6 +17,20 @@ app.configure(function() {
 
 app.get('/api', function(req, res) {
   res.send('Twitch API is running');
+});
+
+app.get('/api/streams/:game', function(req, res) {
+  var options = {
+    protocol: 'https:',
+    host: 'api.twitch.tv/kraken',
+    pathname: '/streams',
+    query: {game: req.params.game, limit: 12}
+  };
+  
+  var query = url.format(options).replace(/%20/g, '+');
+  console.log('RETREIVING: ' + query);
+  
+  request(query).pipe(res);
 });
 
 app.listen(process.env.PORT || 1337);
