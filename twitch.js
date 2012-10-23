0da39d7c0eb5cab0c94a105b63b538c72f858d51
Vincent Samaco <vsamaco@gmail.com>
2012-10-22 19:54:50 -0700

    updated express routes with backbone stream page route

--- twitch.js
+++ twitch.js
@@ -8,7 +8,8 @@ var application_root = __dirname,
 var app = express();
 
 var config = {
-  "elophant_key": "V45wByoYCe2ESQ7h3tnC"
+  "elophant_key": "V45wByoYCe2ESQ7h3tnC",
+  "port": (process.env.PORT || 1337)
 };
 
 app.configure(function() {
@@ -17,6 +18,11 @@ app.configure(function() {
   app.use(app.router);
   app.use(express.static(path.join(application_root, "public")));
   app.use(express.errorHandler({ dumpException: true, showStack: true }));
+});
+
+app.get('/stream/:name', function(req, res) {
+  console.log("streams#name");
+  res.sendfile("public/index.html");
 });
 
 app.get('/api', function(req, res) {
@@ -29,6 +35,19 @@ app.get('/api/streams/:game', function(req, res) {
     host: 'api.twitch.tv/kraken',
     pathname: '/streams',
     query: {game: req.params.game, limit: 12}
+  };
+  
+  var query = url.format(options).replace(/%20/g, '+');
+  console.log('RETREIVING: ' + query);
+  
+  request(query).pipe(res);
+});
+
+app.get('/api/stream/:name', function(req, res) {
+  var options = {
+    protocol: 'https:',
+    host: 'api.twitch.tv/kraken',
+    pathname: '/streams/' + req.params.name,
   };
   
   var query = url.format(options).replace(/%20/g, '+');
@@ -51,5 +70,5 @@ app.get('/api/game/:name', function(req, res) {
   request(query).pipe(res);
 });
 
-app.listen(process.env.PORT || 1337);
-console.log('Running Twitch Server on port ' + (process.env.PORT || 1337));
+app.listen(config.port);
+console.log('Running Twitch Server on port ' + config.port);
